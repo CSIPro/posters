6f8ae08eaaf23a4800e6728a3c68df4982441242
Abraham Hurtado <arhc.hc@gmail.com>
2017-08-28 23:00:54 -0700

    Fixed server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const { resolve } = require('path');
 
@@ -13,22 +12,3 @@ app.listen(process.env.PORT || 3000, (error) => {
   if (error) return;
   console.log(`La aplicación corre en el puerto ${process.env.PORT || 3000}`);
 })
-=======
-var express = require('express');
-
-var app = express();
-
-app.use('/static', express.static(`${__dirname}/build/static`));
-
-app.get('*', (req, res) => {
-  res.sendFile(require('path').resolve(__dirname, './build/index.html'));
-})
-
-app.listen(process.env.PORT || 3000, (error) => {
-  if (error) {
-    return error.message;
-  } else {
-    console.log(`La aplicación está corriendo en el puerto ${process.env.PORT || 3000}`);
-  }
-})
->>>>>>> 1d265f734bafdab85ecb68c49b6b2b87581849cd
